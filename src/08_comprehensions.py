d269bd8fabce3ad574bd211c92b01e9964cf0e17
Ming Liu <mliu@macpro.home>
2019-09-02 21:15:59 -0400

    modify list creatino to comprehension

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,20 +10,14 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-
-for i in range (1, 6):
-    y.append(i)
+y = [i for i in range(1, 6)]
 
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
-
-for i in range (0, 10):
-    y.append(i**3)
+y = [i**3 for i in range(0, 10)]
 
 print(y)
 
@@ -32,9 +26,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
-for i in range (0, len(a)):
-    y.append(a[i].upper())
+y = [word.upper() for word in a]
     
 print(y)
 
@@ -43,10 +35,7 @@ print(y)
 
 x = input("Enter comma-separated numbers: ").split(',')
 
-
 # What do you need between the square brackets to make it work?
-y = []
-for i in range(0, len(x)):
-    if i % 2 == 0:
-        y.append(x[i])
+y = [int(elem,0) for elem in x if int(elem,0) % 2 == 0]
+
 print(y)
